03caded6ce70ce2fae362080f83a2d7849f0718f
taymasoff <taymas97@gmail.com>
2021-05-27 01:33:29 +0300

    initial

---
+++ I Knew It!/I Knew It!Tests/I_Knew_It_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  I_Knew_It_Tests.swift
+//  I Knew It!Tests
+//
+//  Created by Тимур Таймасов on 27.05.2021.
+//
+
+import XCTest
+@testable import I_Knew_It_
+
+class I_Knew_It_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
